8eecb7aa5ce90eb3468dc4af143dc9a60f54424b
SMoore3773 <Smooresculpture@gmail.com>
2020-03-11 22:27:43 -0400

    form and list map added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = props => {
+    console.log(props)
+    return(
+    <div className='toDoListBox'>
+        {props.todos.map(todo => (
+            <Todo
+            key={todo.id}
+            item={todo}
+            />
+        ))}
+    </div>
+    )
+}
+
+export default TodoList;
